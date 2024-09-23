b5879972e0ee9b51cf25552d34a964c00c3fcbde
EduardoRibero <ribeirodomingues2020@gmail.com>
2024-09-23 17:17:18 -0300

    select inner join

--- models/UsersModels.js
+++ models/UsersModels.js
@@ -1,13 +1,49 @@
 import conexao from "../infra/conexao.js";
 
-class UsersModels{
+class UsersModels {
 
-    getAllUsers(){
-        const sql = `select * from usuarios where id <> 1;`
-        return new Promise((resolve, reject)=>{
+    getAllUsers() {
+        const sql = `WITH user_messages AS (
+                        SELECT
+                            m.id,
+                            m.mensagem,
+                            m.insert_time,
+                            m.id_remetente AS user_id
+                        FROM mensagens m
+                        UNION ALL
+                        SELECT
+                            m.id,
+                            m.mensagem,
+                            m.insert_time,
+                            m.id_destinatario AS user_id
+                        FROM mensagens m
+                    ),
+                    ranked_messages AS (
+                        SELECT
+                            user_id,
+                            mensagem,
+                            insert_time,
+                            ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY insert_time DESC) AS rn
+                        FROM user_messages
+                    )
+                    SELECT
+                        u.id,
+                        u.nome,
+                        rm.mensagem,
+                        rm.insert_time
+                    FROM
+                        usuarios u
+                    JOIN
+                        ranked_messages rm ON u.id = rm.user_id
+                    WHERE(
+                        rm.rn = 1 and u.id <> 1)
+                    ORDER BY
+                        rm.insert_time DESC;`
+                        
+        return new Promise((resolve, reject) => {
 
-            conexao.query(sql, (err, resposta)=>{
-                if(err){
+            conexao.query(sql, (err, resposta) => {
+                if (err) {
                     console.log("Erro ao tentar pegar todos os usuarios" + err)
                     reject(err)
                 }
